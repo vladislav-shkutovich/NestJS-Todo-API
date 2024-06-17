5249c642e7bb8ca179dfe86062f692119c6eff22
Vladislav Shkutovich <85294034+vladislav-shkutovich@users.noreply.github.com>
2024-06-17 10:41:06 +0300

    Step 2: Add very basic CRUD API for Todos (file-system approach) (#2)
    * feat: implement the same crud api via file-system approach instead of in-memory approach
    
    * fix: database service imports

--- src/todos/todos.database.service.ts
+++ src/todos/todos.database.service.ts
@@ -1,10 +1,56 @@
-import { Injectable, NotFoundException } from '@nestjs/common'
-import { initialTodos } from './mocks'
+import {
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+  OnModuleInit,
+  OnModuleDestroy,
+} from '@nestjs/common'
+import { resolve } from 'path'
+import { readFile, writeFile } from 'node:fs/promises'
+
 import type { TodoItem, Todos } from 'types/todos'
 
 @Injectable()
-export class TodosDatabaseService {
-  static todos: Todos = initialTodos
+export class TodosDatabaseService implements OnModuleInit, OnModuleDestroy {
+  private static todos: Todos = new Map()
+
+  private readonly todosFilePath: string = resolve(
+    __dirname,
+    '../../../todos.json',
+  )
+
+  async onModuleInit() {
+    await this.loadTodosFromFile()
+  }
+
+  async onModuleDestroy() {
+    await this.saveTodosToFile()
+  }
+
+  private async loadTodosFromFile() {
+    try {
+      const todosFile = await readFile(this.todosFilePath, 'utf8')
+      const todos = JSON.parse(todosFile)
+      for (const id in todos) {
+        if (todos.hasOwnProperty(id)) {
+          TodosDatabaseService.todos.set(id, todos[id])
+        }
+      }
+    } catch (error) {
+      throw new InternalServerErrorException(`Files loading failed. ${error}.`)
+    }
+  }
+
+  private async saveTodosToFile() {
+    try {
+      const todosToJson = JSON.stringify(
+        Object.fromEntries(TodosDatabaseService.todos),
+      )
+      await writeFile(this.todosFilePath, todosToJson, 'utf8')
+    } catch (error) {
+      throw new InternalServerErrorException(`Files saving failed. ${error}.`)
+    }
+  }
 
   create(todoItem: TodoItem): TodoItem {
     TodosDatabaseService.todos.set(todoItem.id, todoItem)
