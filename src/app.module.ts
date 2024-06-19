c1b199ca15cc7ecc3590d691db59d48546133cb1
vladislav-shkutovich <vladislav.shkutovich@gmail.com>
2024-06-19 15:29:47 +0300

    refactor: use nestjs-config instead of dotenv

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,21 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule, ConfigService } from '@nestjs/config'
 import { MongooseModule } from '@nestjs/mongoose'
 import { TodosModule } from './todos/todos.module'
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGOOSE_MODULE_DATABASE_URL'),
+      }),
+      inject: [ConfigService],
+    }),
     TodosModule,
-    MongooseModule.forRoot(process.env.MONGOOSE_MODULE_DATABASE_URL),
   ],
   controllers: [],
   providers: [],
