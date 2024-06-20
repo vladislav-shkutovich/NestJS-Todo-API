2fa4dcbbf1c7968238a806977b43e1b2a3511635
vladislav-shkutovich <vladislav.shkutovich@gmail.com>
2024-06-20 12:02:18 +0300

    fix: custom error classes to handle errors in a more correct way

--- src/common/interceptors/errors.interceptor.ts
+++ src/common/interceptors/errors.interceptor.ts
@@ -8,22 +8,35 @@ import {
 } from '@nestjs/common'
 import { Observable, throwError } from 'rxjs'
 import { catchError } from 'rxjs/operators'
+import { NotFoundError, ValidationError } from '../errors/errors'
 
 @Injectable()
 export class ErrorsInterceptor implements NestInterceptor {
   intercept(_: ExecutionContext, next: CallHandler): Observable<any> {
     return next.handle().pipe(
       catchError((error) => {
-        if (error instanceof HttpException) {
-          return throwError(() => error)
-        } else {
-          return throwError(
-            () =>
-              new HttpException(
-                'Internal server error',
-                HttpStatus.INTERNAL_SERVER_ERROR,
-              ),
-          )
+        switch (true) {
+          case error instanceof NotFoundError:
+            return throwError(
+              () => new HttpException(error.message, HttpStatus.NOT_FOUND),
+            )
+
+          case error instanceof ValidationError:
+            return throwError(
+              () => new HttpException(error.message, HttpStatus.BAD_REQUEST),
+            )
+
+          case error instanceof HttpException:
+            return throwError(() => error)
+
+          default:
+            return throwError(
+              () =>
+                new HttpException(
+                  'Internal server error',
+                  HttpStatus.INTERNAL_SERVER_ERROR,
+                ),
+            )
         }
       }),
     )
--- src/todos/todos.database.service.ts
+++ src/todos/todos.database.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common'
 import { InjectModel } from '@nestjs/mongoose'
 import { Model } from 'mongoose'
+import { NotFoundError, ValidationError } from 'src/common/errors/errors'
 import { TODO_MODEL } from 'src/common/constants/database.constants'
 import type { Todo } from './schemas/todos.schema'
 
@@ -9,25 +10,58 @@ export class TodosDatabaseService {
   constructor(@InjectModel(TODO_MODEL) private todoModel: Model<Todo>) {}
 
   async create(todo: Todo): Promise<Todo> {
+    if (!todo.title || typeof todo.title !== 'string') {
+      throw new ValidationError('Invalid title.')
+    }
+
     const createdTodo = new this.todoModel(todo)
+
     return await createdTodo.save()
   }
 
   async getAll(): Promise<Todo[]> {
-    return await this.todoModel.find().exec()
+    return await this.todoModel.find()
   }
 
   async getById(id: string): Promise<Todo> {
-    return await this.todoModel.findById(id).exec()
+    if (!id || typeof id !== 'string') {
+      throw new ValidationError('Invalid id.')
+    }
+
+    const todoById = await this.todoModel.findById(id)
+
+    if (!todoById) {
+      throw new NotFoundError(`Todo with id ${id} not found`)
+    }
+
+    return todoById
   }
 
   async update(id: string, newParams: Partial<Todo>): Promise<Todo> {
-    return await this.todoModel
-      .findByIdAndUpdate(id, newParams, { new: true })
-      .exec()
+    if (!id || typeof id !== 'string') {
+      throw new ValidationError('Invalid id.')
+    }
+
+    const updatedTodo = await this.todoModel.findByIdAndUpdate(id, newParams, {
+      new: true,
+    })
+
+    if (!updatedTodo) {
+      throw new NotFoundError(`Todo with id ${id} not found`)
+    }
+
+    return updatedTodo
   }
 
   async delete(id: string): Promise<void> {
-    await this.todoModel.findByIdAndDelete(id).exec()
+    if (!id || typeof id !== 'string') {
+      throw new ValidationError('Invalid id.')
+    }
+
+    const deletedTodo = await this.todoModel.findByIdAndDelete(id)
+
+    if (!deletedTodo) {
+      throw new NotFoundError(`Todo with id ${id} not found`)
+    }
   }
 }
