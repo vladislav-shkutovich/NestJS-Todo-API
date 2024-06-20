d026bbb3cb987459e42ba7ede71887957b726b45
vladislav-shkutovich <vladislav.shkutovich@gmail.com>
2024-06-20 16:44:12 +0300

    fix: update todo schema

--- src/todos/schemas/todos.schema.ts
+++ src/todos/schemas/todos.schema.ts
@@ -1,15 +1,17 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose'
-import { HydratedDocument } from 'mongoose'
+import { HydratedDocument, Types } from 'mongoose'
 
 export type TodoDocument = HydratedDocument<Todo>
 
-@Schema()
+@Schema({ versionKey: false })
 export class Todo {
+  _id: Types.ObjectId
+
   @Prop({ required: true })
   title: string
 
   @Prop()
-  description: string
+  description?: string
 }
 
 export const TodoSchema = SchemaFactory.createForClass(Todo)
