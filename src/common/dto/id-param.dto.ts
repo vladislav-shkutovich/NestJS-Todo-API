3e070dc5287ef5ed3791dfccbc27d92e96337862
vladislav-shkutovich <vladislav.shkutovich@gmail.com>
2024-09-02 15:41:09 +0300

    chore: add comment about id-param-dto validation and transformation

--- src/common/dto/id-param.dto.ts
+++ src/common/dto/id-param.dto.ts
@@ -1,6 +1,10 @@
 import { IsMongoId } from 'class-validator'
 import { Types } from 'mongoose'
 
+// TODO: to be discussed at the same time with query params DTO validation
+// topics:
+// - Mongoose Types.ObjectId and ObjectId and MongoDB ObjectId differences
+// - Transformation from string into ObjectId (validation issues)
 export class IdParamDto {
   @IsMongoId()
   id: Types.ObjectId
